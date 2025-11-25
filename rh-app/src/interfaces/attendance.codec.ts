dcb7fdcfb70d2c938276583122a960151e032111
TristanEPSI <tristan.gottschak@laposte.net>
2025-11-25 09:25:46 +0100

    feat add attendance codec & route

--- rh-app/src/interfaces/attendance.codec.ts
+++ rh-app/src/interfaces/attendance.codec.ts
@@ -1,20 +1,41 @@
 import { z } from "zod";
 
-import { BaseEntityCodec } from "./baseEntity.codec.ts";
-import { EmployeeReferenceCodec } from "./employeeReference.codec.ts";
+const timeStringSchema = z
+    .string()
+    .regex(
+        /^([01]\d|2[0-3]):[0-5]\d:[0-5]\d$/,
+        "Doit être au format HH:MM:SS"
+    );
 
-export const AttendanceCodec = BaseEntityCodec.extend({
-    dateTime: z.coerce.date(),
-    clockIn: z.string(),
-    clockOut: z.string(),
-    breakDuration: z.string(),
-    workedHour: z.number(),
+export const attendanceAPICodec = z.object({
+    id: z.number().int(),
+    date: z.string().datetime(),     // "2025-11-26T19:50:34.44Z"
+    clockIn: timeStringSchema,
+    clockOut: timeStringSchema,
+    breakDuration: timeStringSchema,
+    workedHours: z.number(),
     overtimeHours: z.number(),
-    notes: z.string(),
-    employeeId: z.number().optional(),
-    employee: EmployeeReferenceCodec.optional(),
-}).strict();
+    notes: z.string().nullable().optional(),
+    employeeId: z.number().int(),
+    employeeName: z.string(),
+});
+export type AttendanceAPI = z.infer<typeof attendanceAPICodec>;
 
-export const AttendancePayloadCodec = AttendanceCodec.partial();
+export const attendanceCreateCodec = z.object({
+    date: z.string().datetime(),     // "2025-11-26T19:50:34.4400000Z"
+    clockIn: timeStringSchema,
+    clockOut: timeStringSchema,
+    breakDuration: timeStringSchema,
+    notes: z.string().nullable().optional(),
+    employeeId: z.number().int(),
+});
+export type AttendanceCreate = z.infer<typeof attendanceCreateCodec>;
+export const attendanceUpdateCodec = attendanceCreateCodec.partial();
+export type AttendanceUpdate = z.infer<typeof attendanceUpdateCodec>;
 
-export type Attendance = z.infer<typeof AttendanceCodec>;
+// Routes
+// localhost/api/attendance GET & POST
+// .../attendances/employee/:employeeId
+// .../attendances/employee/:emloyeeId/date/2025-11-26 Search By Employee and date (date is an example)
+// .../attendances/date-range?startDate=2025-11-26&endDate=2025-11-27
+// .../attendances/:idAttendance GET OR UPDATE OR DELETE
